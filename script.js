51543cd999f62b2c091fcf325a33a193d3afd8c9
dianafabela8 <fabeladiana.df@gmail.com>
2023-07-27 19:51:27 -0700

    added new feature branch

--- script.js
+++
@@ -1,6 +0,0 @@
-const container = document.querySelector('.image-container');
-
-container.addEventListener('wheel', (event) => {
-  event.preventDefault();
-  container.scrollLeft += event.deltaY;
-});
